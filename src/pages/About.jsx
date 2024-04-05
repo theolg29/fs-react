2119517cc9b330aeb529bcffe8e6f2469e9cae4b
Théo Le Gourrierec <theolg36@gmail.com>
2024-04-05 20:44:37 +0200

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo, exercitationem ea sit quas nostrum quod obcaecati sed. Reiciendis porro deleniti earum in impedit ipsum quos! Optio quidem neque a ab delectus quia, id numquam repellat reiciendis reprehenderit velit, molestias tenetur sapiente suscipit sint consectetur nisi asperiores. Vero, eos? Cumque obcaecati porro dolor, amet accusantium necessitatibus esse enim consequatur libero eos ducimus debitis possimus, doloribus eius. Sequi aliquid repudiandae maiores libero perspiciatis quo, iure eveniet, culpa dignissimos vitae vel nostrum velit dolores? Distinctio vero quo culpa nesciunt, nihil ullam dignissimos, ut enim, rerum hic dolorem? Dolore inventore velit blanditiis debitis, dolores repellat consequuntur nemo perferendis aliquam odio itaque. Sequi similique laborum quod quia pariatur natus quibusdam obcaecati? Ratione, exercitationem fugit. Tenetur, aspernatur sapiente sit molestias aliquam, quos velit ea, esse laboriosam optio in. Commodi laboriosam eveniet iusto eius facere amet minus quod, doloribus obcaecati odio eos magnam, dignissimos dolore harum error unde repudiandae tenetur earum impedit neque tempore quasi sunt cupiditate! Veritatis, magni temporibus quibusdam ab vel omnis, nostrum nobis quam inventore aut tempore blanditiis optio recusandae. Dolores dolorem odit rerum assumenda! Quidem ipsa suscipit rem ducimus voluptate maxime aliquid doloribus alias, iste incidunt sapiente sequi assumenda in aspernatur provident.</p>
+        </div>
+    );
+};
+
+export default About;
