5c9b43cabb2f37fbc3d2ed744da8145d6bdc6d39
Théo Le Gourrierec <Theo@MacBook-Pro.local>
2024-04-24 12:08:41 +0200

    Update: 1:38:00

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import Card from "./Card";
 
 const Countries = () => {
     
     const [data, setData] = useState([]);
+    const [rangeValue, setRangeValue] = useState();
 
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/all").then((res) => setData(res.data));
@@ -11,10 +13,13 @@ const Countries = () => {
     
     return (
         <div className='countries'>
-            <h1>Countries</h1>
+            <ul className="radio-container">
+                <input type="range" min="1" max="250" defaultValue={rangeValue} onChange={(e) => setRangeValue(e.target.value)} />
+                <p>Nombre de pays affiché : {rangeValue}</p>
+            </ul>
             <ul>
                 {
-                data.map((country, index) => (<li key={index}>{country.translations.fra.common}</li>))
+                data.slice(0, rangeValue).map((country, index) => (<Card key={index} country={country} />))
                 }
             </ul>
         </div>
