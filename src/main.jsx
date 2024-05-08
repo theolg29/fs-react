ff14731d58288f29ccc04ce8d3c146ff5d01d9ac
Théo Le Gourrierec <theolg36@gmail.com>
2024-05-08 18:51:18 +0200

    partie 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/index.scss"
+import Lenis from 'lenis'
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+// Ajustez cette valeur pour contrôler la vitesse du défilement
+const scrollSpeed = 1; // Plus la valeur est grande, plus le défilement sera rapide
+
+function raf(time) {
+  lenis.raf(time * scrollSpeed) // Multipliez le temps par la vitesse du défilement
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const Blog = () => {
+
+    const [content, setContent] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (content.length < 140){
+            setError(true);
+        } else {
+            setError(false);
+        }
+
+    }
+
     return (
         <div className="blog-container">
             <Logo />
             <Navigation />
             <h1>Blog</h1>
+
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" placeholder="Nom" />
+                <textarea placeholder="Message" onChange={(e) => setContent(e.target.value)}></textarea>
+                {error && <p>Écrire un minimum de 140 caractères !</p>}
+                <input type="submit" value="Envoyer" />
+            </form>
+
+            <ul>
+
+            </ul>
+
         </div>
     );
 };
